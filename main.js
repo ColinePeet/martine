d070bbd5e1f4a9ee9458aedbfae0ff33dc668910
Coline Peetermans <colpeetermans@gmail.com>
2019-05-12 15:11:47 +0200

    Second version

---
+++ main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: true,
+    },
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
+  });
+
+
+
